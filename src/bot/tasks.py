7c139d8b10d0e61aac096a0486d61002142bc9a9
salahuddinfa <salahuddin@forensicalpha.co>
2024-07-10 10:34:30 +0530

    add the message as task and queue the messages through celery

--- src/bot/tasks.py
+++ src/bot/tasks.py
@@ -4,7 +4,9 @@ import slacky
 from .utils import query_rag
 
 @shared_task
-def slack_message_task(message, channel_id=None, user_id=None, thread_ts=None):
-    response = query_rag(message)
-    r = slacky.send_message(response, channel_id, user_id, thread_ts)
-    return r.status_code
+def slack_message_task(message, channel_id=None, user_id=None, thread_ts=None, image_url=None):
+    response, image_url = query_rag(message)
+    r = slacky.send_message(response, channel_id, user_id, thread_ts, image_url)
+    return r
+
+
